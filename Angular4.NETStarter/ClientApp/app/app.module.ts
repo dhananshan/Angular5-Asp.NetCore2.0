2424d0a7c4472a61f043b4f9f531b1b92c05503e
Navin Dhananshan <Navin Dhananshan>
2017-05-29 01:36:53 +0800

    Add project files.

---
+++ Angular4.NETStarter/ClientApp/app/app.module.ts
@@ -0,0 +1,11 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
